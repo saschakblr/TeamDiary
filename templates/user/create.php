0922bc59cc1b17bef2d95ce57039a107ec6fc9b0
Sascha Kobler <52482687+saschakblr@users.noreply.github.com>
2020-01-22 12:34:56 +0100

    added form

--- templates/user/create.php
+++ templates/user/create.php
@@ -1,21 +1,23 @@
 <!-- four input fields to create a new profile -->
 <div class="card mx-auto" style="width: 30%;">
     <div class="card-body">
-		<div class="form-group">
-	    	<input type="text" name="firstname" placeholder="Firstname" class="form-control">
-		</div>
-		<div class="form-group">
-	        <input type="text" name="Name" placeholder="Name" class="form-control">
-        </div>
-        <div class="form-group">
-	        <input type="text" name="email" placeholder="Email" class="form-control">
-        </div>
-        <div class="form-group">
-	        <input type="text" name="password" placeholder="Password" class="form-control">
-        </div>
-        <div class="form-button">
-            <input type="submit" value="Submit" class="btn btn-success">    
-        </div>
+        <form action="/user/doCreate" method="post">
+            <div class="form-group">
+                <input type="text" name="firstname" placeholder="Firstname" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="text" name="Name" placeholder="Name" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="text" name="email" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="text" name="password" placeholder="Password" class="form-control">
+            </div>
+            <div class="form-button">
+                <input type="submit" value="Submit" class="btn btn-success">    
+            </div>
+        </form>
     </div>
 </div>
 
