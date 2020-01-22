687c6b411410e40d5bdc1c3ea8d5e145b7b086cb
severineschler <60131389+severineschler@users.noreply.github.com>
2020-01-22 10:11:41 +0100

    create profile -- comments

--- templates/user/create.php
+++ templates/user/create.php
@@ -1,6 +1,12 @@
-<!-- two input fields to sign in -->
+<!-- four input fields to create a new profile -->
 <div class="card mx-auto" style="width: 30%;">
     <div class="card-body">
+		<div class="form-group">
+	    	<input type="text" name="firstname" placeholder="Firstname" class="form-control">
+		</div>
+		<div class="form-group">
+	        <input type="text" name="Name" placeholder="Name" class="form-control">
+        </div>
         <div class="form-group">
 	        <input type="text" name="email" placeholder="Email" class="form-control">
         </div>
