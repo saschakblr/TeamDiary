84d143afe530e6f17d69a87b162a49a4b2227acc
severineschler <60131389+severineschler@users.noreply.github.com>
2020-01-22 09:40:13 +0100

    login page

---
+++ templates/user/login.php
@@ -0,0 +1,15 @@
+<div class="card mx-auto" style="width: 30%;">
+    <div class="card-body">
+        <div class="form-group">
+	        <input type="text" name="email" placeholder="Email" class="form-control">
+        </div>
+        <div class="form-group">
+	        <input type="text" name="password" placeholder="Password" class="form-control">
+        </div>
+        <div class="form-button">
+            <input type="submit" value="Submit" class="btn btn-success">    
+        </div>
+    </div>
+</div>
+
+
